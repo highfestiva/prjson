34a399cbc330cb360f7012f7f89b1f733b94b377
Jonas B <jonas.bystrom@hm.com>
2016-11-15 17:14:43 +0100

    JSON pretty-printer.

---
+++ prjson.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from codecs import open, getwriter
+from io import TextIOWrapper
+from json import loads as tojson
+import sys
+from pprint import pprint
+
+sys.stdout = getwriter('ascii')(sys.stdout.detach(), 'replace')
+
+files = [open(f) for f in sys.argv[1:]]
+if not files:
+	files = [TextIOWrapper(sys.stdin.buffer, encoding='utf-8')]
+for f in files:
+	json = tojson(f.read())
+	pprint(json)
